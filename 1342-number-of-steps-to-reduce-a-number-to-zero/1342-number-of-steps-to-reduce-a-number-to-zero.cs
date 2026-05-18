fb5c143ce2131470812730885c53788217a8bd70
Bruno Carvalho <brunocarvalhosoares.dev@gmail.com>
2026-05-17 23:34:52 -0300

    Time: 0 ms (100%), Space: 28.8 MB (83.9%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int NumberOfSteps(int num) 
+    {
+        if(num == 0) return 0;
+
+        int numberOfSteps = 0;
+        while(num != 0)
+        {
+            if(num % 2 == 0)
+            {
+                num = num/2;
+                numberOfSteps += 1;
+            }
+            if(num % 2 == 1)
+            {
+                num -= 1;
+                numberOfSteps += 1;
+            }
+
+            
+        }
+        return numberOfSteps;
+    }
+}
